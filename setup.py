d1c60b78da74522ad72ad2f1372fd95b658643bf
stefanrm@au1.ibm.com <backprop!5>
2019-04-03 13:57:35 +1100

    setup.py and license added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yaat',
+    version='1.0.0',
+    url='https://github.com/maet3608/yaat-dl',
+    author='Stefan Maetschke',
+    author_email='stefan.maetschke@gmail.com',
+    description='Yet another image annotation tool for deep learning',
+    packages=find_packages(),
+    install_requires=[],
+)
